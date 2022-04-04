14f1ea103a1dbe95ab07dcfbcdfbe4df3ad0c76c
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-04-04 09:58:17 -0600

    new

--- Chapter7/src/chapter7/Circle.java
+++ Chapter7/src/chapter7/Circle.java
@@ -1,3 +1,14 @@
+/*
+
+Program: Printing.java          Last Date of this Revision: 06-Mar-2022
+
+Purpose: Create a Printing application that prompts the user for the number of copies to print and then displays the price per copy and the total price for the job.
+
+Author: Ashleen Sidhu, 
+School: CHHS
+Course: Computer Programming 20
+ 
+*/
 package chapter7;
 
 public class Circle 
@@ -9,10 +20,15 @@ public class Circle
 	
 	public static void main(String[] args)
 	{
-		Circle spot = new Circle();
 		
-		spot.setRadius(3);
-		System.out.print("Circle radius: " + spot.getRadius());
-		System.out.print("Circle circumference:: " + spot.circumference());
 	}
 }
+
+
+/* Screen Dump 
+
+Enter the number of copies to be printed: 1001
+Price per copy is: $0.25
+Total cost is: $250.25
+
+ */
