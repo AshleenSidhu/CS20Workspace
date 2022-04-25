2a219d31048688f1398d4a9bb44f0cb858d5a5d6
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-04-25 10:00:31 -0600

    update

--- Chapter7/src/chapter7GUI/Circles.java
+++ Chapter7/src/chapter7GUI/Circles.java
@@ -1,6 +1,43 @@
+/*
+
+Program: Circle.java          Last Date of this Revision: 05-Mar-2022
+
+Purpose:
+
+Author: Ashleen Sidhu, 
+School: CHHS
+Course: Computer Programming 20
+ 
+*/
 package chapter7GUI;
 
 public class Circles 
 {
-
+	private static final double PI = 3.14;
+	private double radius;
+	
+	public Circles() //constructor method
+	{
+		radius = 1; //defult radius
+	}
+	
+	public void setRadius(double newRadius)
+	{
+		radius = newRadius;
+	}
+	
+	public double area()
+	{
+		double circleArea;
+		circleArea = PI * radius * radius;
+		return(circleArea);
+	}
+	
+	public double getRadius()
+	{
+		return(radius);
+	}
+	
+	public static main(String{} {
+ args)
 }
