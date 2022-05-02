68174e2c306122e3660ee9bb2e834123661f47fc
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-05-02 09:38:27 -0600

    up

--- Chapter7/src/chapter7GUI/MySavingsGUI.java
+++ Chapter7/src/chapter7GUI/MySavingsGUI.java
@@ -11,8 +11,6 @@ import javax.swing.JLabel;
 public class MySavingsGUI {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField txtAddA;
 
 	/**
 	 * Launch the application.
@@ -36,6 +34,8 @@ public class MySavingsGUI {
 	public MySavingsGUI() {
 		initialize();
 	}
+	
+	
 
 	/**
 	 * Initialize the contents of the frame.
@@ -47,51 +47,7 @@ public class MySavingsGUI {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBounds(10, 11, 414, 239);
+		panel.setBounds(0, 0, 434, 261);
 		frame.getContentPane().add(panel);
-		
-		textField = new JTextField();
-		textField.setText("Bank");
-		textField.setColumns(10);
-		textField.setBounds(159, 0, 86, 20);
-		panel.add(textField);
-		
-		JButton btnNewButton = new JButton("Menu");
-		btnNewButton.setBounds(23, 39, 89, 23);
-		panel.add(btnNewButton);
-		
-		txtAddA = new JTextField();
-		txtAddA.setColumns(10);
-		txtAddA.setBounds(276, 73, 111, 20);
-		panel.add(txtAddA);
-		
-		JLabel lblNewLabel = new JLabel("1. Show total in bank");
-		lblNewLabel.setBounds(23, 73, 111, 20);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("1. Show total in bank");
-		lblNewLabel_1.setBounds(23, 73, 62, 20);
-		panel.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("2. Add a penny");
-		lblNewLabel_2.setBounds(23, 104, 102, 20);
-		panel.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("3. Add a nickel");
-		lblNewLabel_2_1.setBounds(23, 135, 102, 20);
-		panel.add(lblNewLabel_2_1);
-		
-		JLabel lblNewLabel_2_2 = new JLabel("4. Add a dime");
-		lblNewLabel_2_2.setBounds(23, 166, 102, 20);
-		panel.add(lblNewLabel_2_2);
-		
-		JLabel lblNewLabel_2_3 = new JLabel("5. Add a quarter");
-		lblNewLabel_2_3.setBounds(23, 197, 102, 20);
-		panel.add(lblNewLabel_2_3);
-		
-		JLabel lblNewLabel_2_4 = new JLabel("Enter your choice");
-		lblNewLabel_2_4.setBounds(181, 73, 102, 20);
-		panel.add(lblNewLabel_2_4);
 	}
 }
