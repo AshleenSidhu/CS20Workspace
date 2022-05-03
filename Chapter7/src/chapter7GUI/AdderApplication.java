5507abf343fae0dac7ea783b4c5f7aaa8d6f02f0
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-05-03 11:54:48 -0600

    up

--- Chapter7/src/chapter7GUI/AdderApplication.java
+++ Chapter7/src/chapter7GUI/AdderApplication.java
@@ -11,10 +11,60 @@ Course: Computer Programming 20
 */
 package chapter7GUI;
 
+import java.util.Scanner;
+
 public class AdderApplication 
 {
-	public static void main(String[] args)
+	public static final double Num = Math.random();
+	private int compNum;
+	private double playerGuess;
+	
+	//constructor
+	public AdderApplication()
 	{
-		
+		compNum = (int)(Math.random() * 20 + 0);
+	   
+		playerGuess = 0;
 	}
+	
+	public void setAnswer(double newAnswer)
+	{
+		playerGuess = newAnswer;
+	}
+	
+	public double additionProblem()
+	{
+		double gameNum;
+		
+		gameNum = Num;
+		
+		return(gameNum);
+	}
+	
+	public double getAnswer()
+	{
+		return(playerGuess);
+	}
+	
+	public boolean equals(Object g) //g is a AdderApplication object, determines if the object is equal to another
+	{
+		AdderApplication testObj = (AdderApplication)g;
+		
+		if(testObj.additionProblem() == additionProblem()) //true has been returned if the objects have the same answer
+		{
+			return(true);
+		}
+		
+		else //false has been returned otherwise 
+		{
+			return(false);
+		}
+	}
+	
+	public static void playGame()
+	{
+		System.out.println("");
+	}
+	
+	
 }
