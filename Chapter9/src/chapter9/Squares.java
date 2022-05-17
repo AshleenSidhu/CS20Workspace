d4787d3431ff4ade4d9788bbf6330ec8d362eeee
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-05-17 11:19:03 -0600

    up

--- Chapter9/src/chapter9/Squares.java
+++ Chapter9/src/chapter9/Squares.java
@@ -11,31 +11,63 @@ Course: Computer Programming 20
 */
 package chapter9;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Squares 
 {
-	public static void Squares(int arr[])
+	public static void sortSquares(int arr[])
 	{
-		Scanner input = new Scanner(System.in);
-		
 		int num = arr.length;
 		
 		for (int i = 0; i < num; i++)
 		{
-			System.out.print("");
+			arr[i] = arr[i] * arr[i];
+			Arrays.sort(arr);
 		}
 	}
 	
 	public static void main(String[] args)
 	{
+		int arr[] = {-6, -3, -1, 2, 4, 5};
 		
+		int num = arr.length;
+		
+		for (int i = 0; i < num; i++)
+		{
+			System.out.println(arr[i] + " ");
+		}
+		
+		sortSquares(arr);
+		
+		System.out.println(" ");
+		
+		System.out.println("After sort: ");
+		
+		for (int i = 0; i < num; i++)
+		{
+			System.out.println(arr[i] + " ");
+		}
 		
 	}
-		
+
 }
 
 /* Screen Dump 
+
+-6 
+-3 
+-1 
+2 
+4 
+5 
+ 
+After sort: 
+-3 
+1 
+4 
+5 
+36 
+65536 
 
 
  
