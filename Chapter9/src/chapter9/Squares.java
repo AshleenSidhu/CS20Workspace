b4c14952cf835c813aeb1771b71e83b641e6e334
1005106504 <1005106504@L815WD220016.edu.cbe.ab.ca>
2022-05-17 10:46:22 -0600

    up

--- Chapter9/src/chapter9/Squares.java
+++ Chapter9/src/chapter9/Squares.java
@@ -1,5 +1,42 @@
+/*
+
+Program: .java          Last Date of this Revision: 02-May-2022
+
+Purpose: 
+
+Author: Ashleen Sidhu, 
+School: CHHS
+Course: Computer Programming 20
+ 
+*/
 package chapter9;
 
-public class Squares {
+import java.util.Scanner;
 
+public class Squares 
+{
+	public static void Squares(int arr[])
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int num = arr.length;
+		
+		for (int i = 0; i < num; i++)
+		{
+			System.out.print("");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+		
 }
+
+/* Screen Dump 
+
+
+ 
+*/ 
--- Chapter9/src/chapter9/StudentRoster.java
+++ Chapter9/src/chapter9/StudentRoster.java
@@ -1,3 +1,14 @@
+/*
+
+Program: StudentRoster.java          Last Date of this Revision: 02-May-2022
+
+Purpose: 
+
+Author: Ashleen Sidhu, 
+School: CHHS
+Course: Computer Programming 20
+ 
+*/
 package chapter9;
 
 import java.util.Scanner;
@@ -16,6 +27,7 @@ public class StudentRoster
 		numStudents = input.nextInt();
 		studentNames = new String[numStudents];
 		
+		//generate numbers and store them
 		for (int i = 0; i < studentNames.length; i++)
 		{
 			System.out.print("Enter the name of student " + (i+1) + " in the class: ");
@@ -33,3 +45,16 @@ public class StudentRoster
 		
 	}
 }
+
+
+/* Screen Dump 
+
+Enter the amount of students in the class: 2
+Enter the name of student 1 in the class: name
+Enter the name of student 2 in the class: name2
+
+Student Roster
+name
+name2
+ 
+*/ 
